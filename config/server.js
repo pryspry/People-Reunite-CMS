601158d27dd2fe1498fd622e0737eb05442e0341
Priyo S <psantosa@leverate.co.id>
2021-05-11 14:48:32 +0700

    change url

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://people.xabi.us',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '899ab18082857be54845830ca65046d8'),
